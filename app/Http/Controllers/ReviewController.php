df7b85c81bc32e39770dd0de64f94f5a7aa88995
amrmohy96 <63362021+amrmohy96@users.noreply.github.com>
2020-09-21 17:36:00 +0200

    add paginate , add Review Resource

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -2,15 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\ReviewResource;
+use App\Models\Product;
 use App\Models\Review;
 use Illuminate\Http\Request;
 
 class ReviewController extends Controller
 {
 
-    public function index()
+    public function index(Product $product)
     {
-        //
+        return ReviewResource::collection($product->reviews);
     }
 
 
