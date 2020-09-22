a1e37aa7e718f681ed1c58fec7e0c789b1a5d9df
amrmohy96 <63362021+amrmohy96@users.noreply.github.com>
2020-09-22 02:31:31 +0200

    add new product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,13 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreUpdateProduct;
 use App\Http\Resources\Product\ProductCollection;
 use App\Http\Resources\Product\ProductResource;
 use App\Models\Product;
 use Request;
-
+use Symfony\Component\HttpFoundation\Response;
 class ProductController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth:api')
+            ->except('index', 'show');
+    }
 
     public function index()
     {
@@ -23,9 +29,12 @@ class ProductController extends Controller
     }
 
 
-    public function store(Request $request)
+    public function store(StoreUpdateProduct $request)
     {
-        //
+        $product = Product::create($request->all());
+        return response([
+            'data' => new ProductResource($product)
+        ],Response::HTTP_CREATED);
     }
 
 
